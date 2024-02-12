15587070b795c53ca6122eb6acf742748fbac266
Sebastian Dowell <142616573+SebastianAPCS@users.noreply.github.com>
2024-02-12 01:20:58 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -27,22 +27,67 @@ public class PigLatin {
 	        System.out.println(pigLatin(lines[i]));
 	    }
     }
-    public int findFirstVowel(String sWord) {
-        //precondition: sWord is a valid String of length greater than 0.
-        //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-	    // your code goes here
-        return -1;
+
+    public String pigLatin(String input) {
+        if (!checkForVowels(input)) {
+            input += "ay";
+        } else if (checkForFirstVowel(input)) {
+            input += "way";
+        } else if (checkForQU(input)) {
+            input = appendQU(input);
+        } else if (checkForVowels(input) && !checkForFirstVowel(input)) {
+            input = appendConsonantsAy(input);
+        }
+
+        return input;
     }
 
-    public String pigLatin(String sWord) {
-        //precondition: sWord is a valid String of length greater than 0
-        //postcondition: returns the pig latin equivalent of sWord
-        // more code should go here
-	    if(findFirstVowel(sWord) == -1) {
-		    return sWord + "ay";
-	    }
-	    else {
-		return "ERROR!";
-	    }
+    public static boolean checkForVowels(String input) {
+        String vowels = "aeiou";
+
+        for (int i = 0; i < input.length(); i++) {
+            if (vowels.indexOf(input.substring(i, i + 1)) != -1) {
+                return true;
+            }
+        }
+
+        return false;
     }
+
+    public static boolean checkForFirstVowel(String input) {
+        String vowels = "aeiou";
+
+        if (vowels.indexOf(input.substring(0, 1)) != -1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean checkForQU(String input) {
+        return input.substring(0, 2).equals("qu")
+            || input.substring(0, 2).equals("Qu")
+            || input.substring(0, 2).equals("qU")
+            || input.substring(0, 2).equals("QU");
+    }
+
+    public static String appendQU(String input) {
+        return input.substring(2, input.length()) + input.substring(0, 2) + "ay";
+    }
+
+    public static String appendConsonantsAy(String input) {
+        String consonants = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            if (!checkForVowels(input.substring(i, i + 1))) {
+                consonants += input.substring(i, i + 1);
+            } else {
+                break;
+            }
+        }
+
+        return input.substring(consonants.length()) + consonants + "ay";
+    }
+
+
 }//end PigLatin class
